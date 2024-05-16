75d340b3add0d3e2174d1fc324431f1c6e233877
Bruno Freire <brunoqm99@gmail.com>
2024-05-15 23:06:57 -0400

    chore: Remove unused JavaScript files related to status tap and swipe back gestures

--- public/assets/keyboard2-legacy-BGZPGD1j.js
+++ public/assets/keyboard2-legacy-BGZPGD1j.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CkZLlp4B.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-B3dqSi0z.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
